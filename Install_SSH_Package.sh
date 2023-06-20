3f5634faeef49a43983bb28b7cd76b0d56e5d937
vaibhav-chaudhari6696 <84838129+vaibhav-chaudhari6696@users.noreply.github.com>
2023-06-20 14:12:37 +0530

    Update Install_SSH_Package.sh

--- Install_SSH_Package.sh
+++ Install_SSH_Package.sh
@@ -18,6 +18,7 @@ if [ ! -f ~/.ssh/id_rsa.pub ]; then
     echo "SSH Key pair not found,Generating SSH key pair..."
     ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
     echo "SSH key pair generated."
+    echo -e "--------------------------------------------------------------------------------\n\n"
 fi
 
 
@@ -31,6 +32,8 @@ public_key="\"$public_key\""
 
 # Name of files on the local Linux device
 winexe_binary_file="./winexe"
+local_msi_file_64_bit="./RWSSHDService_x64.msi"
+local_msi_file_32_bit="./RWSSHDService.msi"
 local_msi_file="./RWSSHDService_x64.msi"
 local_ps_script="./msi_install.ps1"
 
@@ -45,9 +48,15 @@ remote_ps_script_name="msi_install.ps1"
 
 
 
-# Check if the MSI file exists
-if [ ! -f "$local_msi_file" ]; then
-    echo "The file $local_msi_file does not exist in the current directory."
+# Check if the MSI file for 64-bit machine exists
+if [ ! -f "$local_msi_file_64_bit" ]; then
+    echo "The file $local_msi_file_64_bit does not exist in the current directory."
+    exit 1
+fi
+
+# Check if the MSI file for 32-bit machine exists
+if [ ! -f "$local_msi_file_32_bit" ]; then
+    echo "The file $local_msi_file_32_bit does not exist in the current directory."
     exit 1
 fi
 
@@ -76,7 +85,38 @@ declare -a devices_list
 
 # Read the devices file into the array
 while IFS=$' \t,' read -r host username password folder || [ -n "$host" ]; do
+
+    # Check if the IP address value present
+    if ! [[ "$host" =~ ^[0-9]+(\.[0-9]+){3}$ ]] || [ -z "$host" ] || [[ "$host" =~ [[:space:]] ]]; then
+        echo "ERROR: Missing IP address, Skipping device."
+        echo -e "--------------------------------------------------------------------------------\n\n"
+        continue
+    fi
+
+    # Check if the username value present
+    if [ -z "$username" ] || [[ "$username" =~ [[:space:]] ]]; then
+        echo "ERROR: Missing username, Skipping device $host"
+        echo -e "--------------------------------------------------------------------------------\n\n"
+        continue
+    fi
+
+    # Check if the password  value present
+    if [ -z "$password" ] || [[ "$password" =~ [[:space:]] ]]; then
+        echo "ERROR: Missing password, Skipping device $host"
+        echo -e "--------------------------------------------------------------------------------\n\n"
+        continue
+    fi
+
+    # Check if the shared folder value present
+    if [ -z "$folder" ]; then
+        echo "ERROR: Missing folder path to copy MSI Package, Skipping device $host"
+        echo -e "--------------------------------------------------------------------------------\n\n"
+        continue
+    fi
+
+
     devices_list+=("$host" "$username" "$password" "$folder")
+
 done < "$devices_file"
 
 
@@ -90,31 +130,7 @@ for ((i=0; i<${#devices_list[@]}; i+=4)); do
         folder="${devices_list[$i+3]}"
 
 
-        # Check if the IP address value present
-        if ! [[ "$host" =~ ^[0-9]+(\.[0-9]+){3}$ ]] || [ -z "$host" ] || [[ "$host" =~ [[:space:]] ]]; then
-            echo "Error: Missing IP address, Skipping device"
-            continue
-        fi
-
-        # Check if the username value present
-        if [ -z "$username" ] || [[ "$username" =~ [[:space:]] ]]; then
-            echo "Error: Missing username, Skipping device"
-            continue
-        fi
-
-        # Check if the password  value present
-        if [ -z "$password" ] || [[ "$password" =~ [[:space:]] ]]; then
-            echo "Error: Missing password, Skipping device"
-            continue
-        fi
-
-        # Check if the shared folder value present
-        if [ -z "$folder" ]; then
-            echo "Error: Missing folder path to copy MSI Package, Skipping device"
-            continue
-        fi
-
-
+       
 
         echo -e "###########################################################################"
         echo "Machine"
@@ -122,43 +138,53 @@ for ((i=0; i<${#devices_list[@]}; i+=4)); do
         echo -e "###########################################################################\n"
 
 
-        echo -e "------------ REACHABLE PORT CHECK ------------"
+        echo -e "\n------------ REACHABLE PORT CHECK ------------\n"
 
         # Check for port 445 open
         nc -z -w 10 "$host" "445"
 
         # If given port is not open on remote device then skip that device
         if [ "$?" -ne 0 ]; then
-            echo "Machine $host is not reachable over port 445, skipping checks for machine"
+            echo "Machine $host is not reachable over port 445, skipping checks for machine."
+            echo -e "--------------------------------------------------------------------------------\n\n"
             continue
         else
-            echo "Machine $host is reachable over port 445"
+            echo "Machine $host is reachable over port 445."
         fi
 
 
         #Check the architecture of the remote Windows device using winexe
-        arch_result=$(./winexe -U "$username%$password" //"$host" "wmic os get osarchitecture"  | awk 'NR==2')
+        arch_result=$(./winexe -U "$username%$password" //"$host" "wmic os get osarchitecture" )
 
         if [ $? -ne 0 ]; then
-            echo "Error executing winexe command for device"
-            echo "$arch_result"
+            echo "ERROR: Failed to Get architecture of remote host."
+            echo -e "--------------------------------------------------------------------------------\n\n"
             continue
         fi
+
+        arch_result=$(echo $arch_result || awk 'NR==2')
         
         # Set Installation Directory according architecture of the remote Windows device
         if [[ $arch_result == *"64-bit"* ]]; then
+            local_msi_file="\"$local_msi_file_64_bit\""
+            remote_msi_file_name="RWSSHDService_x64.msi"
             install_dir='"C:\Program Files (x86)"'
+
         else
+            local_msi_file="\"$local_msi_file_32_bit\""
+            remote_msi_file_name="RWSSHDService.msi"
             install_dir='"C:\Program Files"'
         fi
+
 
 
         # Get actual system path of shared folder, required to access MSI Package while installation  
         netshare_output=$(./winexe -U "$username%$password" //"$host" "net share $folder")
 
         if [ $? -ne 0 ]; then
-            echo "Error executing winexe command for device"
-            echo "$netshare_output"
+            echo "ERROR: Failed to Get system folder path of shared folder."
+            echo -e "--------------------------------------------------------------------------------\n\n"
+
             continue
         fi
 
@@ -178,58 +204,63 @@ for ((i=0; i<${#devices_list[@]}; i+=4)); do
         ps_script_path="${folder_system_path}\\${remote_ps_script_name}"
         ps_script_path="\"$ps_script_path\""
 
-        echo -e "\n"
 
+        echo -e "\n------------ Copying MSI Package and PowerShell Script to remote host  ----------\n"
 
         # Copy the Powershell Script file to the remote Windows device        
         if ! smbclient -U "$username%$password" "//${host}/$folder" -c "put "$local_ps_script" "$remote_ps_script_name""; then
             echo "Failed to copy $local_ps_script to $host"
+            echo -e "--------------------------------------------------------------------------------\n\n"
             continue
+        else
+            echo "Successfully copied PowerShell Script File to $host"
         fi
+
+
+        echo -e "\n--------------------------------------------------------------------------------\n"
 
 
         # Copy the SSH MSI file to the remote Windows device
         if ! smbclient -U "$username%$password" "//${host}/$folder" -c "put "$local_msi_file" "$remote_msi_file_name""; then
             echo "Failed to copy $local_msi_file to $host"
+            echo -e "--------------------------------------------------------------------------------\n\n"
             continue
+        else
+            echo "Successfully copied MSI Package File to $host"
         fi
 
 
-        echo -e "\n"
-        
+
+        echo -e "\n--------------------------------------------------------------------------------\n"
+
 
         # Execute the Powershell Script on the remote Windows device
         output=$(./winexe -U "$username%$password" //"$host" "powershell -ExecutionPolicy Bypass -File $ps_script_path  -Username $username -Password $password -Port $port -Publickey $public_key  -InstallDir $install_dir -PackagePath $msi_package_path")
 
         if [ $? -ne 0 ]; then
-            echo "Error executing winexe command for device"
-            echo "$output"
+            echo "ERROR: Failed to Run PowerShell Script on remote host host."
+            echo -e "--------------------------------------------------------------------------------\n\n"
             continue
         fi
 
         echo "$output"
 
         
-        echo -e "\n"
+        echo -e "\n--------------------------------------------------------------------------------\n"
 
-        ssh_known_host_path=$(echo ~/.ssh/known_hosts)
-        ssh_path="\"$ssh_path\""
-        ssh-keygen -f "$ssh_known_host_path" -R "$host"
-
-        echo -e "\n\n"
 
         # Execute the SSH command to check the success of the connection
-        ssh_output=$(ssh -n -o StrictHostKeyChecking=no -o ConnectTimeout=30 -o BatchMode=yes "$username@$host" "echo Success")   
+        ssh -n -o StrictHostKeyChecking=no -o ConnectTimeout=30 -o BatchMode=yes "$username@$host" "exit" 2>/dev/null   
 
 
-        if [[ $ssh_output == *"Success"* ]]; then
+        if [ $? -eq 0 ]; then
             echo "SSH package installed successfully with all parameters on $host. "
         else
             echo "ERROR: SSH package is not installed successfully with all parameters on $host. OR Machine Not reachable over port 22"
         fi
 
 
-        echo -e "----------------------------------------------------------------------------------"
+        echo -e "\n----------------------------------------------------------------------------------"
         echo -e "----------------------------------------------------------------------------------\n\n\n"
 
 
